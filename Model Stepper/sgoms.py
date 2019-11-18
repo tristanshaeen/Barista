a0819db042d74ba3f80593fb9f8d59ee62bbdf36
Rob <rlwest@gmail.com>
2019-11-18 12:49:44 -0500

    added report function

--- Model Stepper/sgoms.py
+++ Model Stepper/sgoms.py
@@ -7,6 +7,9 @@ class Sgoms:
         try:        
             if key.char == 'd':
                 return False # Stop listener - or listener.stop??
+            if key.char == 'r':
+                print('report')
+                self.report()
             if key.char == 'w':
                 print(5555555555555555555)
                 self.step()
@@ -27,3 +30,18 @@ class Sgoms:
 
     def step(self):
         print('step')
+
+
+    def report(self):
+          for pu in self.planning_units:
+            print ('planning unit = ', end=' ')
+            print (pu)
+            for ut in self.planning_units[pu]:
+              print ('   unit task = ', end=' ')
+              print (ut)
+              for m in self.unit_tasks[ut]:
+                print ('      method = ', end=' ')
+                print (m)
+
+
+            
